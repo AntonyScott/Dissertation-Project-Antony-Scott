cfa0f3ea80cd54ead58cc416b0112c4c21682556
AntonyScott <antonyscott1@hotmail.co.uk>
2023-02-17 18:21:37 +0000

    Commit 6
    Creating main menu for project. Following tutorial at: https://www.youtube.com/watch?v=Cq_Nnw_LwnI. Currently at 41.55

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEditor.Rendering;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header ("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSliderValue = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Confirmation Prompt")]
+    [SerializeField] private GameObject confirmationPrompt;
+
     [Header("Levels to load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -34,5 +44,36 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        //show prompt
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSliderValue.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
