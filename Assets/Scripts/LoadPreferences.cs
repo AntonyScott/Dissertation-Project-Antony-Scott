c30547f9b6e1b631c792b2a4d0bd94302a1a8d4a
AntonyScott <antonyscott1@hotmail.co.uk>
2023-03-24 21:11:02 +0000

    Commit 23
    Added a load preferences script to main menu, also made it so the title in the introduction scene has a separate timer to the other text boxes.

--- Assets/Scripts/LoadPreferences.cs
+++ Assets/Scripts/LoadPreferences.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+//this script initialises values for the main menu
 public class LoadPreferences : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("General Setting")]
+    [SerializeField] private bool canUse = false;
+    [SerializeField] private MainMenu menuController;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [Header("Brightness Setting")]
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+
+    [Header("Quality Level Setting")]
+    [SerializeField] private TMP_Dropdown qualityDropdown = null;
+
+    [Header("Fullscreen Setting")]
+    [SerializeField] private Toggle fullscreenToggle;
+
+    private void Awake()
     {
-        
+        if (canUse)
+        {
+            if (PlayerPrefs.HasKey("masterVolume"))
+            {
+                float localVolume = PlayerPrefs.GetFloat("masterVolume");
+                volumeSlider.value = localVolume;
+                AudioListener.volume = localVolume;
+            }
+            else
+            {
+                menuController.ResetButton("Audio");
+            }
+
+            if (PlayerPrefs.HasKey("masterQuality"))
+            {
+                int localQuality = PlayerPrefs.GetInt("masterQuality");
+                qualityDropdown.value = localQuality;
+                QualitySettings.SetQualityLevel(localQuality);
+            }
+
+            if (PlayerPrefs.HasKey("masterFullscreen"))
+            {
+                int localFullscreen = PlayerPrefs.GetInt("masterFullScreen");
+                if(localFullscreen == 1)
+                {
+                    Screen.fullScreen = true;
+                    fullscreenToggle.isOn = true;
+                }
+                else
+                {
+                    Screen.fullScreen= false;
+                    fullscreenToggle.isOn = false;
+                }
+            }
+
+            if (PlayerPrefs.HasKey("masterBrightness"))
+            {
+                float localBrightness = PlayerPrefs.GetFloat("masterBrightness");
+
+                brightnessTextValue.text = localBrightness.ToString("0.0");
+                brightnessSlider.value = localBrightness;
+                //changes the brightness to local brightness
+            }
+        }
     }
 }
--- Assets/Scripts/TextIntroduction.cs
+++ Assets/Scripts/TextIntroduction.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class TextIntroduction : MonoBehaviour
 {
     public TextMeshProUGUI[] texts;
     public float fadeInDuration = 1.0f;
     public float fadeOutDuration = 1.0f;
+    public float firstTextBoxDuration = 5.0f;
     public float delayBetweenTransitions = 5.0f;
 
     private int currentIndex = 0;
@@ -18,16 +20,52 @@ public class TextIntroduction : MonoBehaviour
         StartCoroutine(FadeInSequence());
     }
 
+    private void Update()
+    {
+        // Check if the space bar has been pressed to skip to the next scene
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            // Load the next scene
+            SceneManager.LoadScene("Overworld");
+        }
+    }
+
     private IEnumerator FadeInSequence()
     {
+        // Handle the first text box separately
+        if (currentIndex < texts.Length)
+        {
+            TextMeshProUGUI currentText = texts[currentIndex];
+            yield return FadeText(currentText, 0f, 1f, fadeInDuration);
+            yield return new WaitForSeconds(firstTextBoxDuration);
+            yield return FadeText(currentText, 1f, 0f, fadeOutDuration);
+            currentIndex++;
+        }
+
         while (currentIndex < texts.Length)
         {
             TextMeshProUGUI currentText = texts[currentIndex];
             yield return FadeText(currentText, 0f, 1f, fadeInDuration);
-            yield return new WaitForSeconds(delayBetweenTransitions);
+
+            // Wait for a set amount of time or until the space bar is pressed
+            float timer = 0f;
+            while (timer < delayBetweenTransitions)
+            {
+                if (Keyboard.current.spaceKey.wasPressedThisFrame)
+                {
+                    // Load the next scene
+                    SceneManager.LoadScene("Overworld");
+                }
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
             yield return FadeText(currentText, 1f, 0f, fadeOutDuration);
             currentIndex++;
         }
+
+        // Load the next scene when all texts have been displayed
         SceneManager.LoadScene("Overworld");
     }
 
@@ -38,7 +76,7 @@ public class TextIntroduction : MonoBehaviour
         startColor.a = startAlpha;
         text.color = startColor;
 
-        while (Time.time < startTime + duration) 
+        while (Time.time < startTime + duration)
         {
             float t = (Time.time - startTime) / duration;
             Color currentColor = text.color;
@@ -46,6 +84,7 @@ public class TextIntroduction : MonoBehaviour
             text.color = currentColor;
             yield return null;
         }
+
         Color endColor = text.color;
         endColor.a = endAlpha;
         text.color = endColor;
